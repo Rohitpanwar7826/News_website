b8aeccff54cb4f3e94e8930ebf77da283f67cb2c
Rohit singh pawar <rohitpawar5894@gmail.com>
2021-07-16 15:22:25 +0530

    Initial commit

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.element1', {
+    strings: [
+
+        'Rohit Singh Pawar',
+        'A IT Engineer'
+
+    ],
+    typeSpeed: 130,
+    smartBackspace: true,
+    backDelay: 190,
+    loop: true,
+    loopCount: Infinity,
+
+
+});
