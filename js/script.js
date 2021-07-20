c88cb676def97533a9e1a7b62a4f8415c900de57
Rohit singh pawar <rohitpawar5894@gmail.com>
2021-07-20 19:30:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -122,8 +122,9 @@ const show_data = (element) => {
                 <h2 id="read" class="title-font mt-5 overflow-clip whitespace-wrap overflow-hidden text-lg font-medium"id="title1">
                 ${description}
                 </h2>
-                <Button id="${i}" onclick="data_get(this.id)"  class="mt-10" >Read more</Button>
+                
             </div>
+            <Button id="${i}" onclick="data_get(this.id)"  class="mt-10" >Read more</Button>
         </div>
         `;
 
@@ -148,7 +149,7 @@ page.onchange = function (event) {
 }
 const up = () => {
     window.scroll({
-        top: 470,
+        top: 530,
         behavior: 'smooth'
     });
 }
@@ -182,39 +183,39 @@ const data_get = (index = -1, ...args) => {
                     hide.style.display = "";
                 }
                 let ones_news = `
-<div data-aos="zoom-in"  data-aos-delay="600" data-aos-duration="1000"  data-aos-once="false" data-aos-offset="200" id="hide_news" class="shadow lg:w-5/6 md:border-0 lg:border-t-0 lg:border-r-0 lg:border-l-0 mx-auto lg:border-2 lg:border-red-900">
-    <div class="rounded-lg h-64 overflow-hidden">
-        <img alt="content" class="object-cover object-center h-full w-full"
-            src="${element[index]['image']['url']}">
-</div>
-        <div class="flex flex-col sm:flex-row mt-10">
-            <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                <div
-                    class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                        <circle cx="12" cy="7" r="4"></circle>
-                    </svg>
-                </div>
-                <div class="flex flex-col items-center text-center justify-center">
-                    <h2 class="font-medium title-font mt-4 text-gray-950 text-lg">Rohit singh Pawar</h2>
-                    <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                    <p class="text-base ml-9 text-gray-950">${element[index]['title']}.</p>
-                </div>
-            </div>
-            <div
-                class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                <p class="leading-relaxed text-gray-950 text-lg mb-4 break-normal mr-4">${element[index]['body']}.</p>
-                <a class="text-red-400 inline-flex items-center" href="${element[index]['url']}" target="_blank">Learn More
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                </a>
-            </div>
-        </div>
-    </div>`
+                    <div data-aos="zoom-in"  data-aos-delay="600" data-aos-duration="1000"  data-aos-once="false" data-aos-offset="200" id="hide_news" class="shadow lg:w-5/6 md:border-0 lg:border-t-0 lg:border-r-0 lg:border-l-0 mx-auto lg:border-2 lg:border-red-900">
+                        <div class="rounded-lg h-64 overflow-hidden">
+                            <img alt="content" class="object-cover object-center h-full w-full"
+                                src="${element[index]['image']['url']}">
+                    </div>
+                            <div class="flex flex-col sm:flex-row mt-10">
+                                <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                                    <div
+                                        class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                                            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                            <circle cx="12" cy="7" r="4"></circle>
+                                        </svg>
+                                    </div>
+                                    <div class="flex flex-col items-center text-center justify-center">
+                                        <h2 class="font-medium title-font mt-4 text-gray-950 text-lg">Rohit singh Pawar</h2>
+                                        <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                                        <p class="text-base ml-4 mr-4 text-gray-950 ">${element[index]['title']}.</p>
+                                    </div>
+                                </div>
+                                <div
+                                    class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                                    <p class="leading-relaxed text-gray-950 text-lg mb-4 break-normal mr-4 ml-2">${element[index]['body'].slice(0, 900)}.</p>
+                                    <a class="text-red-400 inline-flex items-center ml-3" href="${element[index]['url']}" target="_blank">Learn More
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>`
                 one_news.innerHTML = ones_news;
                 up()
             }).catch(function (error) {
